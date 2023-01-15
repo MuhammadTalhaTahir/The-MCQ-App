be759c9874ca3844213e3ea7561968df38bfef52
unknown <bitf19a003@pucit.edu.pk>
2023-01-15 15:41:14 +0500

    launching the quiz activity

--- app/src/main/java/com/example/themcqapp/MenuScreen.java
+++ app/src/main/java/com/example/themcqapp/MenuScreen.java
@@ -2,13 +2,39 @@ package com.example.themcqapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuScreen extends AppCompatActivity {
-
+public class MenuScreen extends AppCompatActivity implements View.OnClickListener{
+    Button startButton, resultButton, linkButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+        startButton = findViewById(R.id.startQuiz);
+        resultButton = findViewById(R.id.viewResult);
+        linkButton = findViewById(R.id.gitRepoLink);
+        startButton.setOnClickListener(this);
+        resultButton.setOnClickListener(this);
+        linkButton.setOnClickListener(this);
+    }
+    private int generateSession(){
+        return -1;
+    }
+    @Override
+    public void onClick(View view){
+        switch (view.getId()){
+            case(R.id.startQuiz):
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra("sessionId", this.generateSession());
+                startActivity(intent);
+                break;
+            case(R.id.viewResult):
+                break;
+            case(R.id.gitRepoLink):
+                break;
+        }
     }
 }
